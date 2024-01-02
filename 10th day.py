c8252d22c6a49448c08382b8913b5b829a0ab190
SHAIK KHASIM  BASHA <115825218+khasimbasha5757@users.noreply.github.com>
2024-01-02 20:06:39 +0530

    Add files via upload

---
+++ 10th day.py
@@ -0,0 +1,36 @@
+# # a=int(input("Give me your age : "))
+# # # conditioal operators
+# # if(a>18):
+# #           print("You can drive")
+# # else:
+# #         print("YOu can not drive")
+# # a=100
+# # b=200
+# # if(a>=100):
+# #           print("Yes it is correct!")
+# #           print("good")
+# # else:
+# #         print("no u r not correct")
+# # print("Bad")
+# apple=190 + 10
+# budget=200
+# if(apple<budget):
+#           print("Yes u can buy it")
+# elif(apple==budget):
+#         print("it is equal f u buy it u wont have any other money")
+# else:
+#         print("Do not take apples")
+# print("Yes at last u bought the apples")
+
+time=int(input("What's the time now: "))
+if(time<12 ):
+          print("Good morning! ")
+elif(time>12 and time<16):
+        print("Good Afternoon! ")
+elif(time>16 and time<20):
+        print("Good evening! ")
+elif(time>20 and time<24):
+        print("Good night! ")
+else:
+        print("You are giving the wrong time sir")
+print("Have a nice day.")
